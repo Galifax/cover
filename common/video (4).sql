b0d6abb37aa1b8f9838e8e1a945e223dc28db4d1
Galifax <galifax94@gmail.com>
2017-07-12 23:00:47 +0300

    sql

---
+++ common/video (4).sql
@@ -0,0 +1,264 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Час створення: Лип 12 2017 р., 22:59
+-- Версія сервера: 5.7.16
+-- Версія PHP: 7.1.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База даних: `video`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблиці `likes`
+--
+
+CREATE TABLE `likes` (
+  `id` int(11) NOT NULL,
+  `profile_id` int(11) NOT NULL,
+  `video_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп даних таблиці `likes`
+--
+
+INSERT INTO `likes` (`id`, `profile_id`, `video_id`) VALUES
+(1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблиці `migration`
+--
+
+CREATE TABLE `migration` (
+  `version` varchar(180) NOT NULL,
+  `apply_time` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп даних таблиці `migration`
+--
+
+INSERT INTO `migration` (`version`, `apply_time`) VALUES
+('m000000_000000_base', 1499683713),
+('m130524_201442_init', 1499683716);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблиці `profile`
+--
+
+CREATE TABLE `profile` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `nickname` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `surname` varchar(255) NOT NULL,
+  `age` int(11) NOT NULL,
+  `date_registerated` timestamp NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп даних таблиці `profile`
+--
+
+INSERT INTO `profile` (`id`, `user_id`, `nickname`, `name`, `surname`, `age`, `date_registerated`) VALUES
+(1, 1, 'Galifax', 'Андрей', 'Середюк', 23, '2017-07-11 21:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблиці `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `auth_key` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password_reset_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `status` smallint(6) NOT NULL DEFAULT '10',
+  `created_at` int(11) NOT NULL,
+  `updated_at` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Дамп даних таблиці `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `auth_key`, `password_hash`, `password_reset_token`, `email`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'agalio94', 'xcvuDfB8766n_WMprf83TtCj4A6azHtZ', '$2y$13$Me2rfy5JREI4spefUzjWQO27J2yWNHjDPmuDZHhTFUVMxC5pInMaa', NULL, 'galifax94@gmail.com', 10, 1499865374, 1499865374);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблиці `video`
+--
+
+CREATE TABLE `video` (
+  `id` int(11) NOT NULL,
+  `profile_id` int(11) NOT NULL,
+  `src` varchar(55) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(1000) NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп даних таблиці `video`
+--
+
+INSERT INTO `video` (`id`, `profile_id`, `src`, `name`, `description`, `date`) VALUES
+(1, 1, 'uploads/1.mp4', 'qqq', 'qqqq', '2017-07-12 00:00:00'),
+(2, 1, 'uploads/1.mp4', '222', '2222', '2017-07-14 00:00:00'),
+(3, 1, 'uploads/1.mp4', '33333', 'qweqwqeq', '2017-07-14 00:00:00'),
+(4, 1, 'uploads/1.mp4', 'qwewqqwrewr', 'rwerewrewr', '2017-07-25 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблиці `view`
+--
+
+CREATE TABLE `view` (
+  `id` int(11) NOT NULL,
+  `video_id` int(11) NOT NULL,
+  `views` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп даних таблиці `view`
+--
+
+INSERT INTO `view` (`id`, `video_id`, `views`) VALUES
+(1, 1, 74),
+(2, 3, 9),
+(3, 3, 10);
+
+--
+-- Індекси збережених таблиць
+--
+
+--
+-- Індекси таблиці `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `profile_id` (`profile_id`),
+  ADD KEY `video_id` (`video_id`);
+
+--
+-- Індекси таблиці `migration`
+--
+ALTER TABLE `migration`
+  ADD PRIMARY KEY (`version`);
+
+--
+-- Індекси таблиці `profile`
+--
+ALTER TABLE `profile`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Індекси таблиці `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `password_reset_token` (`password_reset_token`);
+
+--
+-- Індекси таблиці `video`
+--
+ALTER TABLE `video`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `profile_id` (`profile_id`),
+  ADD KEY `profile_id_2` (`profile_id`);
+
+--
+-- Індекси таблиці `view`
+--
+ALTER TABLE `view`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `video_id` (`video_id`);
+
+--
+-- AUTO_INCREMENT для збережених таблиць
+--
+
+--
+-- AUTO_INCREMENT для таблиці `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT для таблиці `profile`
+--
+ALTER TABLE `profile`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT для таблиці `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT для таблиці `video`
+--
+ALTER TABLE `video`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT для таблиці `view`
+--
+ALTER TABLE `view`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- Обмеження зовнішнього ключа збережених таблиць
+--
+
+--
+-- Обмеження зовнішнього ключа таблиці `likes`
+--
+ALTER TABLE `likes`
+  ADD CONSTRAINT `likes_ibfk_1` FOREIGN KEY (`profile_id`) REFERENCES `profile` (`id`),
+  ADD CONSTRAINT `likes_ibfk_2` FOREIGN KEY (`video_id`) REFERENCES `video` (`id`);
+
+--
+-- Обмеження зовнішнього ключа таблиці `profile`
+--
+ALTER TABLE `profile`
+  ADD CONSTRAINT `profile_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
+-- Обмеження зовнішнього ключа таблиці `video`
+--
+ALTER TABLE `video`
+  ADD CONSTRAINT `video_ibfk_1` FOREIGN KEY (`profile_id`) REFERENCES `user` (`id`);
+
+--
+-- Обмеження зовнішнього ключа таблиці `view`
+--
+ALTER TABLE `view`
+  ADD CONSTRAINT `view_ibfk_1` FOREIGN KEY (`video_id`) REFERENCES `video` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
