ee8625397ff0a6191560187a95b3592c21ca6e23
LtFrager <sadesade2011@gmail.com>
2017-07-14 12:48:15 +0300

    Style-2

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -25,41 +25,44 @@ AppAsset::register($this);
 <body>
 <?php $this->beginBody() ?>
 
-<div class="wrap" style="padding-bottom: 0px; background-color: #f99500 ">
-
+<div class="wrap color-1" style="padding-bottom: 0px;  ">
+<!--    background-color: #f99500-->
     <!-- Second navbar for sign in -->
-    <nav class="navbar navbar-default" style="margin-bottom: 60px">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-2">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Brand</a>
-            </div>
 
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="navbar-collapse-2">
-                <ul class="nav navbar-nav navbar-right">
-                    <form class="navbar-form navbar-left">
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Search">
-                        </div>
-                        <button type="submit" class="btn btn-default">Submit</button>
-                    </form>
-                    <li><a href="">Войти</a></li>
-                </ul>
-            </div><!-- /.navbar-collapse -->
-        </div><!-- /.container -->
-    </nav><!-- /.navbar -->
 
 
 
     <div class="">
-            <div class="container-fluid background-gray" style="margin-left: 60px;margin-right: 60px;">
+            <div class="container-fluid color-3" style="margin-left: 60px;margin-right: 60px;margin-top: 60px">
+
+                <nav class="navbar navbar-default" style="margin-top: 10px">
+                    <div class="container">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-2">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <a class="navbar-brand" href="#">Brand</a>
+                        </div>
+
+                        <!-- Collect the nav links, forms, and other content for toggling -->
+                        <div class="collapse navbar-collapse" id="navbar-collapse-2">
+                            <div class="col-md-8 col-md-push-3">
+                            <form class="navbar-form navbar-left">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" placeholder="Найти видео" size="45">
+                                    </div>
+                                <button type="submit" class="btn btn-default">
+                                    <i class="fa fa-search" aria-hidden="true"></i>
+                                    Найти</button>
+                            </form>
+                        </div>
+                        </div><!-- /.navbar-collapse -->
+                    </div><!-- /.container -->
+                </nav><!-- /.navbar -->
 
                 <div class="profile-sidebar col-md-2">
                     <!-- SIDEBAR USERPIC -->
@@ -156,7 +159,7 @@ AppAsset::register($this);
                 <!--index END MENU -->
         </div>
 
-        <div class="clearfix" style="background-color: white"></div>
+        <div class="clearfix"></div>
 
         <div class="container-fluid nopadding background-gray "   style=" padding: 60;margin-right: 60px;margin-left: 60px; margin-bottom: 60px" >
             <?= Breadcrumbs::widget([
