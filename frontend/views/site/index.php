810b7c21a217f8132f679b2aa7fc9ce1ff9fbcdc
LtFrager <sadesade2011@gmail.com>
2017-07-20 14:04:16 +0300

    bla2

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -96,6 +96,38 @@ $this->title = 'My Yii Application';
                 <span class="duration">03:15</span>
             </a>
         </li>
+        <li class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+            <a href="#" title="Claudio Bravo, antes su debut con el Barça en la Liga">
+                <img src="http://i.ytimg.com/vi/ZKOtE9DOwGE/mqdefault.jpg" alt="Barca" class="img-responsive" height="130px" />
+                <h2>Claudio Bravo, antes su debut con el Barça en la Liga</h2>
+                <span class="glyphicon glyphicon-play-circle"></span>
+                <span class="duration">03:15</span>
+            </a>
+        </li>
+        <li class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+            <a href="#" title="Claudio Bravo, antes su debut con el Barça en la Liga">
+                <img src="http://i.ytimg.com/vi/ZKOtE9DOwGE/mqdefault.jpg" alt="Barca" class="img-responsive" height="130px" />
+                <h2>Claudio Bravo, antes su debut con el Barça en la Liga</h2>
+                <span class="glyphicon glyphicon-play-circle"></span>
+                <span class="duration">03:15</span>
+            </a>
+        </li>
+        <li class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+            <a href="#" title="Claudio Bravo, antes su debut con el Barça en la Liga">
+                <img src="http://i.ytimg.com/vi/ZKOtE9DOwGE/mqdefault.jpg" alt="Barca" class="img-responsive" height="130px" />
+                <h2>Claudio Bravo, antes su debut con el Barça en la Liga</h2>
+                <span class="glyphicon glyphicon-play-circle"></span>
+                <span class="duration">03:15</span>
+            </a>
+        </li>
+        <li class="col-lg-3 col-md-4 col-sm-4 col-sm-4 col-xs-12">
+            <a href="#" title="Claudio Bravo, antes su debut con el Barça en la Liga">
+                <img src="http://i.ytimg.com/vi/ZKOtE9DOwGE/mqdefault.jpg" alt="Barca" class="img-responsive" height="130px" />
+                <h2>Claudio Bravo, antes su debut con el Barça en la Liga</h2>
+                <span class="glyphicon glyphicon-play-circle"></span>
+                <span class="duration">03:15</span>
+            </a>
+        </li>
     </ul>
 </div>
 
@@ -119,14 +151,6 @@ $this->title = 'My Yii Application';
             </a>
         </li>
         <li class="col-lg-3 col-sm-4 col-xs-12">
-            <a href="#" title="Claudio Bravo, antes su debut con el Barça en la Liga">
-                <img src="http://i.ytimg.com/vi/ZKOtE9DOwGE/mqdefault.jpg" alt="Barca" class="img-responsive" height="130px" />
-                <h2>Claudio Bravo, antes su debut con el Barça en la Liga</h2>
-                <span class="glyphicon glyphicon-play-circle"></span>
-                <span class="duration">03:15</span>
-            </a>
-        </li>
-        <li class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
             <a href="#" title="Claudio Bravo, antes su debut con el Barça en la Liga">
                 <img src="http://i.ytimg.com/vi/ZKOtE9DOwGE/mqdefault.jpg" alt="Barca" class="img-responsive" height="130px" />
                 <h2>Claudio Bravo, antes su debut con el Barça en la Liga</h2>
